e4e4633b40f8e3a10229d8f04527632258fdeff9
Tetyana <tsergata@gmail.com>
2020-01-14 16:36:32 +0100

    Struttura iniziale

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // 1.Il computer genera 16 numeri casuali da 1 a 100.
+var arrayNumUser = [];
+
+for (var i = 0; i < 4; i++) {
+  var numUser = parseInt(prompt('Inserisci un numero da 1 a 100'));
+  console.log(numUser);
+}
+
+
+var arrayNumPc = [];
+
+for (var i = 0; i < 16; i++) {
+  var numRandom = getRandomNumber (1, 100);
+  arrayNumPc.push(numRandom);
+  console.log(arrayNumPc);
+
+}
+// Funzione
+function getRandomNumber (min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  var random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return random;
+}
