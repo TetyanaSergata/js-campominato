7340edd86a16a67c058b9ea278930172ba31d023
Tetyana <tsergata@gmail.com>
2020-01-15 09:27:54 +0100

    Prove

--- js/script.js
+++ js/script.js
@@ -4,22 +4,40 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // 1.Il computer genera 16 numeri casuali da 1 a 100.
-var arrayNumUser = [];
-
-for (var i = 0; i < 4; i++) {
-  var numUser = parseInt(prompt('Inserisci un numero da 1 a 100'));
-  console.log(numUser);
-}
-
-
 var arrayNumPc = [];
-
 for (var i = 0; i < 16; i++) {
   var numRandom = getRandomNumber (1, 100);
+  console.log(numRandom);
   arrayNumPc.push(numRandom);
-  console.log(arrayNumPc);
 
 }
+console.log(arrayNumPc);
+
+// 2. Chiedo all’utente di inserire per 84 volte un numero da 1 a 100
+// 3. Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+var arrayNumUser = [];
+for (var i = 0; i < 84; i++) {
+  var numUser = parseInt(prompt('Inserisci un numero da 1 a 100'));
+  console.log(numUser);
+  arrayNumUser.push(numUser);
+}
+
+//
+// while (numUser !== arrayNumPc[i]){
+//   arrayNumUser.push(numUser);
+//   numUser = parseInt(prompt('Inserisci un numero da 1 a 100'));
+// }
+
+if(numUser !== arrayNumPc[i]) {
+  console.log(arrayNumUser);
+  arrayNumUser.push(numUser);
+}else if (numUser == arrayNumPc[i] ) {
+  alert('numero presente');
+}
+
+
+
+
 // Funzione
 function getRandomNumber (min, max){
   min = Math.ceil(min);
